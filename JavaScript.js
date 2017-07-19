4eaea540ef84eb6c033dd559e2ef18522292bd4a
Woodsmike <Woodsmike@users.noreply.github.com>
2017-07-19 19:32:03 -0400

    added jscript for collapse menu

---
+++ JavaScript.js
@@ -0,0 +1,8 @@
+﻿function myFunction() {
+    var x = document.getElementById("navvy");
+    if (x.className === "navbar navbar-static-top") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar navbar-static-top";
+    }
+}
